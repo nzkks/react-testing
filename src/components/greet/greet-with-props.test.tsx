05ac8ed2d2a05b774a9a9790bc2aa157601562e9
nzkks <kkshanthosh@gmail.com>
2024-04-13 20:47:46 +1200

    Since grouping the tests, the test names can have a cleaner texts

--- src/components/greet/greet-with-props.test.tsx
+++ src/components/greet/greet-with-props.test.tsx
@@ -5,13 +5,13 @@ import { render, screen } from '@testing-library/react';
 import GreetWithProps from './greet-with-props';
 
 describe('Greet', () => {
-  test('Greet renders correctly', () => {
+  test('renders correctly', () => {
     render(<GreetWithProps />);
     const textElement = screen.getByText('Hello');
     expect(textElement).toBeInTheDocument();
   });
 
-  test('Greet renders with name', () => {
+  test('renders with name', () => {
     render(<GreetWithProps name="NZKKS" />);
     const textElement = screen.getByText('Hello NZKKS');
     expect(textElement).toBeInTheDocument();
