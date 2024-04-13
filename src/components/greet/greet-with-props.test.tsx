4ac27e566203c66021d274ede58ca011ce1db7a6
nzkks <kkshanthosh@gmail.com>
2024-04-13 20:46:48 +1200

    Grouping tests within a file (test suite) by using describe and wrapping the necessary tests

--- src/components/greet/greet-with-props.test.tsx
+++ src/components/greet/greet-with-props.test.tsx
@@ -4,14 +4,16 @@
 import { render, screen } from '@testing-library/react';
 import GreetWithProps from './greet-with-props';
 
-test('Greet renders correctly', () => {
-  render(<GreetWithProps />);
-  const textElement = screen.getByText('Hello');
-  expect(textElement).toBeInTheDocument();
-});
+describe('Greet', () => {
+  test('Greet renders correctly', () => {
+    render(<GreetWithProps />);
+    const textElement = screen.getByText('Hello');
+    expect(textElement).toBeInTheDocument();
+  });
 
-test('Greet renders with name', () => {
-  render(<GreetWithProps name="NZKKS" />);
-  const textElement = screen.getByText('Hello NZKKS');
-  expect(textElement).toBeInTheDocument();
+  test('Greet renders with name', () => {
+    render(<GreetWithProps name="NZKKS" />);
+    const textElement = screen.getByText('Hello NZKKS');
+    expect(textElement).toBeInTheDocument();
+  });
 });
