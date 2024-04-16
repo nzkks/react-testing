b32a4278627ef07d82b83c053ce38c4d7e1854ee
nzkks <kkshanthosh@gmail.com>
2024-04-16 13:51:41 +1200

    custom render function

--- src/components/mui/mui-mode.test.tsx
+++ src/components/mui/mui-mode.test.tsx
@@ -1,12 +1,9 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen } from '../../test-utils';
 import { MuiMode } from './mui-mode';
-import { AppProviders } from '../../providers/AppProviders';
 
 describe('MuiMode', () => {
   test('renders text correctly', () => {
-    render(<MuiMode />, {
-      wrapper: AppProviders
-    });
+    render(<MuiMode />);
 
     expect(screen.getByRole('heading')).toHaveTextContent('dark mode');
   });
