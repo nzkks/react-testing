c7d3ea5840838b8b3466f518b1a4fedcc65063a7
nzkks <kkshanthosh@gmail.com>
2024-04-18 19:27:26 +1200

    MSW Error handling

--- src/components/users/users.test.tsx
+++ src/components/users/users.test.tsx
@@ -1,4 +1,6 @@
 import { render, screen } from '@testing-library/react';
+import { rest } from 'msw';
+import { server } from '../../mocks/server';
 import { Users } from './users';
 
 describe('Users', () => {
@@ -10,7 +12,20 @@ describe('Users', () => {
 
   // test('renders a list of users', async () => {
   //   render(<Users />);
-  //   const users = await screen.findByRole('listitem');
-  //   expect(users).toHaveLength(3);
+  //   expect(await screen.findAllByRole('listitem')).toHaveLength(3);
   // });
+
+  test('renders error', async () => {
+    server.use(
+      rest.get(
+        'https://jsonplaceholder.typicode.com/users',
+        (req, res, ctx) => {
+          return res(ctx.status(500));
+        }
+      )
+    );
+
+    render(<Users />);
+    expect(await screen.findByText('Error fetching users')).toBeInTheDocument();
+  });
 });
